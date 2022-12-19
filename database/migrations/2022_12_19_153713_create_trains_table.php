ce1889c9d9235fa86779c24402d521334050c206
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-12-19 20:29:20 +0100

    added migration and created table on db

---
+++ database/migrations/2022_12_19_153713_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company',50);
+            $table->string('departure_station',50);
+            $table->string('arrival_station',50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code',10);
+            $table->integer('number_carriages');
+            $table->boolean('in_time');
+            $table->boolean('late');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
